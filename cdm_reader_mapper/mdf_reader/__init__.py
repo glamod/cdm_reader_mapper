1c0362ecbe840c980b0b64532caf6462df0e81d6
Ludwig Lierhammer <ludwig.lierhammer@dwd.de>
2024-02-14 11:10:26 +0100

    delete complex reading functions

--- cdm_reader_mapper/mdf_reader/__init__.py
+++ cdm_reader_mapper/mdf_reader/__init__.py
@@ -3,8 +3,5 @@
 from __future__ import annotations
 
 from .code_tables import code_tables  # noqa
-from .get_sections import get_sections  # noqa
-from .import_data import import_data  # noqa
 from .read import read  # noqa
-from .read_sections import read_sections  # noqa
 from .schema import schemas  # noqa
