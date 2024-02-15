03d3a127959fe04a43b1ee4ceab753a3218c106e
Ludwig Lierhammer <ludwig.lierhammer@dwd.de>
2024-02-15 15:11:52 +0100

    shorten test data (n=5)

--- cdm_reader_mapper/data/__init__.py
+++ cdm_reader_mapper/data/__init__.py
@@ -12,40 +12,42 @@ class test_data:
         self.data_path = os.path.dirname(os.path.abspath(__file__))
 
         self.test_063_714 = self._get_data_dict(
-            "063-714_2010-07_subset.imma", "imma1_d714"
+            "063-714_2010-07-01_subset.imma", "imma1_d714"
         )
         self.test_069_701 = self._get_data_dict(
-            "069-701_1845-04_subset.imma", "imma1_d701"
+            "069-701_1845-04-01_subset.imma", "imma1_d701"
         )
         self.test_084_706 = self._get_data_dict(
-            "084-706_1919-03_subset.imma", "imma1_d705-707"
+            "084-706_1919-03-01_subset.imma", "imma1_d705-707"
         )
         self.test_085_705 = self._get_data_dict(
-            "085-705_1938-04_subset.imma", "imma1_d705-707"
+            "085-705_1938-04-01_subset.imma", "imma1_d705-707"
         )
         self.test_096_702 = self._get_data_dict(
-            "096-702_1873-01_subset.imma", "imma1_d702"
+            "096-702_1873-01-01_subset.imma", "imma1_d702"
         )
         self.test_098_707 = self._get_data_dict(
-            "098-707_1916-04_subset.imma", "imma1_d705-707"
+            "098-707_1916-04-01_subset.imma", "imma1_d705-707"
         )
         self.test_103_794 = self._get_data_dict(
-            "103-794_2021-11_subset.imma", "imma1_nodt"
+            "103-794_2021-11-01_subset.imma", "imma1_nodt"
         )
         self.test_125_704 = self._get_data_dict(
-            "125-704_1878-10_subset.imma", "imma1_d704"
+            "125-704_1878-10-01_subset.imma", "imma1_d704"
         )
         self.test_125_721 = self._get_data_dict(
-            "125-721_1862-06_subset.imma", "imma1_d721"
+            "125-721_1862-06-01_subset.imma", "imma1_d721"
         )
         self.test_133_730 = self._get_data_dict(
-            "133-730_1776-10_subset.imma", "imma1_d730"
+            "133-730_1776-10-01_subset.imma", "imma1_d730"
         )
         self.test_143_781 = self._get_data_dict(
-            "143-781_1987-09_subset.imma", "imma1_d781"
+            "143-781_1987-09-01_subset.imma", "imma1_d781"
         )
-        self.test_144_703 = self._get_data_dict("144-703_1979-09_subset.imma", "imma1")
-        self.test_gcc_mix = self._get_data_dict("mix_out_200302.immt", "gcc_immt")
+        self.test_144_703 = self._get_data_dict(
+            "144-703_1979-09-01_subset.imma", "imma1"
+        )
+        self.test_gcc_mix = self._get_data_dict("mix_out_20030201.immt", "gcc_immt")
 
     def _get_data_dict(self, data_file, schema):
         return {
