2c09d8bafb93145f71357b065b18a4db497b861d
Ludwig Lierhammer <ludwig.lierhammer@dwd.de>
2024-03-08 09:09:47 +0100

    always load md5 file

--- cdm_reader_mapper/common/getting_files.py
+++ cdm_reader_mapper/common/getting_files.py
@@ -3,9 +3,10 @@ from __future__ import annotations
 
 import hashlib
 import os
+import warnings
+
 from pathlib import Path
 from urllib.request import urlretrieve
-
 from platformdirs import user_cache_dir
 
 try:
@@ -13,7 +14,7 @@ try:
 except ImportError:
     from importlib_resources import files as _files
 
-_default_cache_dir_ = Path(user_cache_dir("cdm-testdata"))
+_default_cache_dir_ = Path(user_cache_dir("cdm-testdata", ".cache"))
 
 
 def _file_md5_checksum(f_name):
@@ -23,12 +24,22 @@ def _file_md5_checksum(f_name):
     return hash_md5.hexdigest()
 
 
-def _get_remote_file(file, url, name):
+def _get_remote_file(lfile, url, name):
     url = "/".join((url, name.as_posix()))
-    file.parent.mkdir(exist_ok=True, parents=True)
+    lfile.mkdir(exist_ok=True, parents=True)
+    return urlretrieve(url, lfile)
 
-    return urlretrieve(url, file)
-
+def _check_md5s(f, md5, mode="error"):
+    msg = f"{f.as_posix()} and md5 checksum do not match."
+    md5_ = _file_md5_checksum(f)
+    if md5_.strip() != md5.strip():
+      f.unlink()
+      if mode == "error":
+        raise OSError(msg)
+      elif mode == "warning":
+        warnings.warn(msg)
+        return
+    return True
 
 def _get_file(
     name: Path,
@@ -39,61 +50,62 @@ def _get_file(
     cache_dir = cache_dir.absolute()
     cache_dir.mkdir(exist_ok=True, parents=True)
     local_file = cache_dir / name
-    md5_name = name.with_suffix(".md5")
+    md5_name = name.with_suffix(f"{suffix}.md5")
     md5_file = cache_dir / md5_name
-
+    
+    _get_remote_file(md5_file, url, md5_name)    
+    with open(md5_file) as f:
+        remote_md5 = f.read()   
+         
     if not local_file.is_file():
         _get_remote_file(local_file, url, name)
-
-    if not md5_file.is_file():
-        _get_remote_file(md5_file, url, md5_name)
-
-    local_md5 = _file_md5_checksum(local_file)
-    with open(md5_file) as f:
-        remote_md5 = f.read()
-
-    if local_md5.strip() != remote_md5.strip():
-        local_file.unlink()
-        raise OSError(f"{local_file.as_posix()} and md5 checksum do not match.")
+        local_md5 = _file_md5_checksum(local_file)
+        _check_md5s(local_file, remote_md5)       
+    else:
+        if not _check_md5s(local_file, remote_md5, mode="warning"):
+          _get_remote_file(local_file, url, name)
+          _check_md5s(local_file, remote_md5)          
 
     return local_file
 
-
+# idea copied from xclim that it raven that it borrowed from xclim that borrowed it from xarray that was borrowed from Seaborn
 def load_file(
     name: str | os.PathLike,
-    deck: str,
     github_url: str = "https://github.com/glamod/cdm-testdata",
     branch: str = "main",
-    mode: str = "input",
-    suffix: str = "imma",
+    suffix: str = ".imma",
     cache: bool = True,
     cache_dir: Path = _default_cache_dir_,
 ):
     """Load file from the online Github-like repository.
-
+    
     Parameters
     ----------
-    name
-    deck
-    github_url
-    branch
-    mode
-    suffix
-    cache
-    cache_dir
-
+    name : str or os.PathLike
+        Name of the file containing the dataset.
+    github_url : str
+        URL to GitHub repository where the data is stored.
+    branch : str, optional
+        For GitHub-hosted files, the branch to download from.
+    cache_dir : Path
+        The directory in which to search for and write cached data.
+    cache : bool
+        If True, then cache data locally for use on subsequent calls.
+        
     Returns
     -------
-    None.
+    Path
     """
     if isinstance(name, str):
         name = Path(name)
 
+    name = name.with_suffix(suffix)
+    
     if not github_url.lower().startswith("http"):
         raise ValueError(f"GitHub URL not safe: '{github_url}'.")
         return
 
-    url = "/".join((github_url, "raw", branch, deck, mode))
+    url = "/".join((github_url, "raw", branch))
 
     local_file = _get_file(
         name=name,
