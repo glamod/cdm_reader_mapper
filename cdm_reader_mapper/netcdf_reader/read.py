6c1842c7fe3d0ecfe24d5b2c6f4ffd912f4b2554
Ludwig Lierhammer <ludwig.lierhammer@dwd.de>
2024-03-18 14:32:01 +0100

    new class NetCDFFileader; similar to MDFFileReader

--- cdm_reader_mapper/netcdf_reader/read.py
+++ cdm_reader_mapper/netcdf_reader/read.py
@@ -4,11 +4,24 @@ from __future__ import annotations
 
 from xarray import open_mfdataset
 
+
+class NetCDFFileReader:
+    def __init__(self, source):
+        self.source = source
+
+    def read(self):
+        ds = open_mfdataset(self.source)
+        self.data = ds.to_dataframe().reset_index()
+        self.attrs = ds.attrs
+        self.mask = self.data.copy()
+        self.dtypes = ds.dtypes
+        return self
+
+
 def read(
-  source,
-  data_mode=None,
-  data_mode_path=None,
-  **kwargs,
+    source,
+    data_mode=None,
+    data_mode_path=None,
+    **kwargs,
 ):
-  ds = open_mfdataset(source)
-  return ds
+    return NetCDFFileReader(source).read()
