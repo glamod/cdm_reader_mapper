c48ad7f4d58852ce8ab210ee6bea8289ca332a01
ludwig.lierhammer <ludwig.lierhammer@dwd.de>
2024-05-03 09:45:32 +0100

    import load_tables from tables.tables_hdlr

--- cdm_reader_mapper/cdm_mapper/__init__.py
+++ cdm_reader_mapper/cdm_mapper/__init__.py
@@ -5,3 +5,6 @@ from .mapper import map_model  # noqa
 from .table_reader import read_tables  # noqa
 from .table_writer import cdm_to_ascii  # noqa
 from .table_writer import table_to_ascii  # noqa
+from .tables.tables_hdlr import tables_hdlr
+
+load_tables = tables_hdlr().load_tables  # noqa
