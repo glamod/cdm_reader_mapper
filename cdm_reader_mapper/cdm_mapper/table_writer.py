860603568aaedd7f25deb02274700702ded04036
ludwig.lierhammer <ludwig.lierhammer@dwd.de>
2024-05-24 07:59:51 +0100

    convert float to int

--- cdm_reader_mapper/cdm_mapper/table_writer.py
+++ cdm_reader_mapper/cdm_mapper/table_writer.py
@@ -51,7 +51,7 @@ def print_integer(data, null_label):
     def _return_str(x, null_label):
         if pd.isna(x):
             return null_label
-        return str(int(x))
+        return str(int(float(x)))  # ValueError: invalid literal for int() with base 10: '5.0'
 
     return data.apply(lambda x: _return_str(x, null_label))
 
