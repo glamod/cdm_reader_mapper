d5fb27ebed659e5d44df697130ce95de9449a97a
Ludwig Lierhammer <ludwig.lierhammer@dwd.de>
2024-06-26 12:31:50 +0200

    remove unused import

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -5,7 +5,7 @@ import pytest  # noqa
 
 from cdm_reader_mapper.operations import corrections, inspect, replace, select
 
-from ._data import attrs_df, data_df, mask_df
+from ._data import data_df, mask_df
 from ._results import correction_df, table_df
 
 
