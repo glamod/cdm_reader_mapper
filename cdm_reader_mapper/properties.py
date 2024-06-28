5e533bd25fe070e26880bd6f0f43b1d85e149190
Ludwig Lierhammer <ludwig.lierhammer@dwd.de>
2024-06-28 08:43:03 +0200

    do not differentiate between both integer and float types

--- cdm_reader_mapper/properties.py
+++ cdm_reader_mapper/properties.py
@@ -2,35 +2,12 @@
 
 from __future__ import annotations
 
-numpy_integers = [
-    "int8",
-    "int16",
-    "int32",
-    "int64",
-    "uint8",
-    "uint16",
-    "uint32",
-    "uint64",
-]
-numpy_floats = ["float16", "float32", "float64"]
+numpy_integers = "int"
+numpy_floats = "float"
 
-pandas_nan_integers = {
-    "int8": "Int8",
-    "int16": "Int16",
-    "int32": "Int32",
-    "int64": "Int64",
-    "uint8": "UInt8",
-    "uint16": "UInt16",
-    "uint32": "UInt32",
-    "uint64": "UInt64",
-}
-
-numeric_types = numpy_integers.copy()
-numeric_types.extend(numpy_floats)
-numeric_types.extend(pandas_nan_integers.values())
+numeric_types = [numpy_integers, numpy_floats]
 
 object_types = ["str", "object", "key", "datetime"]
 
 data_types = object_types.copy()
-data_types.extend(numpy_integers)
-data_types.extend(numpy_floats)
+data_types.extend(numeric_types)
