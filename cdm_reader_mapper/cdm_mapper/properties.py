7d12e8fdc89bf25cba4ccea87a5f778480060d1d
Ludwig Lierhammer <ludwig.lierhammer@dwd.de>
2024-06-28 10:03:43 +0200

    use float instead of float64

--- cdm_reader_mapper/cdm_mapper/properties.py
+++ cdm_reader_mapper/cdm_mapper/properties.py
@@ -43,7 +43,7 @@ pandas_dtypes["from_atts"].update({x: x for x in numeric_types})
 # ...from CDM table definitions psuedo-sql(...) --------------------------------
 pandas_dtypes["from_sql"] = {}
 pandas_dtypes["from_sql"]["timestamp with timezone"] = "object"
-pandas_dtypes["from_sql"]["numeric"] = "float64"
+pandas_dtypes["from_sql"]["numeric"] = "float"
 pandas_dtypes["from_sql"]["int"] = "int"
 
 # Some defaults ---------------------------------------------------------------
