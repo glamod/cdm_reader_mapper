ed3a2d5fe92d5ffd71aaaceda8b199b353814339
Ludwig Lierhammer <ludwig.lierhammer@dwd.de>
2024-09-25 10:14:00 +0200

    fixging: call test data

--- tests/_data.py
+++ tests/_data.py
@@ -12,6 +12,6 @@ def _read_data(**kwargs):
     return read_.data, read_.attrs, read_.mask
 
 
-data_dict = dict(test_data.test_063_714)
+data_dict = dict(test_data.test_icoads_r300_d714)
 data_df, attrs_df, mask_df = _read_data(**data_dict)
 data_pa, attrs_pa, mask_pa = _read_data(chunksize=10000, **data_dict)
