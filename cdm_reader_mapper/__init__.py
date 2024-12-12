9ce38aaa5344f6a51a757ab2c0f608c3396b8006
Ludwig Lierhammer <ludwig.lierhammer@dwd.de>
2024-12-12 09:47:15 +0100

    add functions to init

--- cdm_reader_mapper/__init__.py
+++ cdm_reader_mapper/__init__.py
@@ -3,14 +3,16 @@
 from __future__ import annotations
 
 from . import cdm_mapper  # noqa
-from .cdm_mapper import read_tables  # noqa
-from .cdm_mapper.properties import cdm_tables  # noqa
 from . import common  # noqa
 from . import mdf_reader  # noqa
-from .mdf_reader import read as read_mdf  # noqa
 from . import metmetpy  # noqa
 from . import operations  # noqa
+from .cdm_mapper import cdm_to_ascii  # noqa
+from .cdm_mapper import map_model  # noqa
+from .cdm_mapper import read_tables  # noqa
+from .cdm_mapper.properties import cdm_tables  # noqa
 from .data import test_data  # noqa
+from .mdf_reader import read as read_mdf  # noqa
 
 __author__ = """Ludwig Lierhammer"""
 __email__ = "ludwiglierhammer@dwd.de"
