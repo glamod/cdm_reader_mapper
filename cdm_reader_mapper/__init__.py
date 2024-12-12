5ae9a817466beb89a88fe9a2e53c246bb19c0b44
Ludwig Lierhammer <ludwig.lierhammer@dwd.de>
2024-12-12 08:39:29 +0100

    add directly to cdm_mapper init

--- cdm_reader_mapper/__init__.py
+++ cdm_reader_mapper/__init__.py
@@ -3,8 +3,11 @@
 from __future__ import annotations
 
 from . import cdm_mapper  # noqa
+from .cdm_mapper import read_tables  # noqa
+from .cdm_mapper.properties import cdm_tables  # noqa
 from . import common  # noqa
 from . import mdf_reader  # noqa
+from .mdf_reader import read as read_mdf  # noqa
 from . import metmetpy  # noqa
 from . import operations  # noqa
 from .data import test_data  # noqa
