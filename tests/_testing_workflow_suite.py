8590dcd319e2d1a9c0210507726fc1ad0deb5384
Ludwig Lierhammer <ludwig.lierhammer@dwd.de>
2024-12-18 15:30:00 +0100

    rename test_cdm to test_workflow

--- tests/_testing_workflow_suite.py
+++ tests/_testing_workflow_suite.py
@@ -1,90 +1,20 @@
 from __future__ import annotations
 
-import ast
 import os
 
 import pandas as pd
 
-from cdm_reader_mapper import mdf_reader, read_mdf, read_tables
+from cdm_reader_mapper import read_mdf, read_tables
 from cdm_reader_mapper.common.pandas_TextParser_hdlr import make_copy
 
 from ._results import result_data
-
-
-def _pandas_read_csv(
-    *args,
-    delimiter=mdf_reader.properties.internal_delimiter,
-    squeeze=False,
-    name=False,
-    **kwargs,
-):
-    df = pd.read_csv(
-        *args,
-        **kwargs,
-        quotechar="\0",
-        escapechar="\0",
-        delimiter=delimiter,
-    )
-    if squeeze is True:
-        df = df.squeeze()
-
-    if name is not False:
-        df.name = name
-
-    return df
-
-
-def _evaluate_columns(columns):
-    columns_ = []
-    for col in columns:
-        try:
-            columns_.append(ast.literal_eval(col))
-        except ValueError:
-            columns_.append(col)
-    return columns_
-
-
-def _read_result_data(data_file, columns, **kwargs):
-    columns_ = _pandas_read_csv(data_file, nrows=0).columns
-    columns_ = _evaluate_columns(columns_)
-    data_ = _pandas_read_csv(
-        data_file,
-        names=columns_,
-        skiprows=1,
-        **kwargs,
-    )
-    return data_[columns]
-
-
-def drop_rows(df, drops):
-    if drops == "all":
-        return df.drop(df.index)
-    elif drops:
-        return df.drop(drops).reset_index(drop=True)
-    return df
-
-
-def get_col_subset(output, codes_subset):
-    col_subset = []
-    if codes_subset is None:
-        return col_subset
-    for subset in codes_subset:
-        for column in output.columns:
-            if subset in column:
-                col_subset.append(column)
-    return col_subset
-
-
-def remove_datetime_columns(output, output_, col_subset):
-    del output[("header", "record_timestamp")]
-    del output[("header", "history")]
-    del output_[("header", "record_timestamp")]
-    del output_[("header", "history")]
-
-    if len(col_subset) > 0:
-        output = output[col_subset]
-        output_ = output_[col_subset]
-    return output, output_
+from ._utilities import (
+    drop_rows,
+    get_col_subset,
+    pandas_read_csv,
+    read_result_data,
+    remove_datetime_columns,
+)
 
 
 def _testing_suite(
@@ -118,13 +48,13 @@ def _testing_suite(
     if not os.path.isfile(result_data_file):
         return
 
-    data_exp = _read_result_data(
+    data_exp = read_result_data(
         result_data_file,
         read_.columns,
         dtype=read_.dtypes,
         parse_dates=read_.parse_dates,
     )
-    mask_exp = _read_result_data(expected_data["mask"], read_.columns)
+    mask_exp = read_result_data(expected_data["mask"], read_.columns)
 
     data_exp = drop_rows(data_exp, drops)
     mask_exp = drop_rows(mask_exp, drops)
@@ -143,7 +73,7 @@ def _testing_suite(
         return
 
     if val_dt is not None:
-        val_dt_ = _pandas_read_csv(
+        val_dt_ = pandas_read_csv(
             expected_data["vadt"],
             header=None,
             squeeze=True,
@@ -153,7 +83,7 @@ def _testing_suite(
         pd.testing.assert_series_equal(val_dt, val_dt_, check_dtype=False)
 
     if val_id is not None:
-        val_id_ = _pandas_read_csv(
+        val_id_ = pandas_read_csv(
             expected_data["vaid"],
             header=None,
             squeeze=True,
---
+++ tests/test_workflow.py
@@ -0,0 +1,197 @@
+from __future__ import annotations
+
+import os
+
+import pytest  # noqa
+
+from cdm_reader_mapper import test_data
+
+from ._testing_workflow_suite import _testing_suite
+
+
+@pytest.mark.parametrize(
+    "imodel, cdm_subset, codes_subset, out_path, mapping, drops, mdf_kwargs",
+    [
+        ("icoads_r300_d714", None, None, ".", True, None, {}),
+        (
+            "icoads_r300_d714",
+            None,
+            None,
+            None,
+            False,
+            None,
+            {"sections": ["core", "c99"]},
+        ),
+        ("icoads_r300_d701_type1", None, None, None, True, None, {}),
+        ("icoads_r300_d701_type2", None, None, None, True, None, {}),
+        ("icoads_r300_d702", None, None, None, True, None, {}),
+        ("icoads_r300_d703", None, None, None, True, None, {}),
+        ("icoads_r300_d704", None, None, None, True, None, {}),
+        ("icoads_r300_d705", None, None, None, True, None, {}),
+        ("icoads_r300_d706", None, None, None, True, None, {}),
+        ("icoads_r300_d707", None, None, None, True, None, {}),
+        ("icoads_r300_d721", None, None, None, True, None, {}),
+        ("icoads_r300_d730", None, None, None, True, None, {}),
+        ("icoads_r300_d781", None, None, None, True, None, {}),
+        ("icoads_r302_d794", None, None, None, True, None, {}),
+        ("icoads_r300_d201", None, None, None, True, None, {}),
+        ("icoads_r300_d892", None, None, None, True, None, {}),
+        ("icoads_r300_d700", None, None, None, True, None, {}),
+        ("icoads_r302_d792", None, None, None, True, None, {}),
+        ("icoads_r302_d992", None, None, None, True, None, {}),
+        ("gcc", None, None, None, True, None, {}),
+        ("craid", None, None, None, True, None, {}),
+        (
+            "icoads_r300_d714",
+            ["header", "observations-sst"],
+            None,
+            None,
+            True,
+            None,
+            {},
+        ),
+        (
+            "icoads_r300_d714",
+            None,
+            ["platform_sub_type", "ship_speed_ms"],
+            None,
+            True,
+            None,
+            {},
+        ),
+        ("icoads_r300_d714", None, None, None, True, None, {"chunksize": 3}),
+        (
+            "icoads_r300_d714",
+            None,
+            None,
+            None,
+            False,
+            None,
+            {"sections": ["c99"], "chunksize": 3},
+        ),
+        (
+            "icoads_r300_d702",
+            None,
+            None,
+            None,
+            True,
+            [0, 1, 2, 3, 4],
+            {"year_init": 1874},
+        ),
+        (
+            "icoads_r300_d702",
+            None,
+            None,
+            None,
+            True,
+            [5, 6, 7, 8, 9],
+            {"year_end": 1874},
+        ),
+        (
+            "icoads_r300_d702",
+            None,
+            None,
+            None,
+            True,
+            "all",
+            {"year_init": 1874, "year_end": 1874},
+        ),
+        (
+            "gcc",
+            None,
+            None,
+            None,
+            True,
+            [0, 1, 2, 3, 4],
+            {"year_init": 2002},
+        ),
+        (
+            "craid",
+            None,
+            None,
+            None,
+            True,
+            "all",
+            {"year_end": 2003},
+        ),
+        (
+            "icoads_r300_d703",
+            None,
+            None,
+            None,
+            False,
+            None,
+            {
+                "ext_schema_path": os.path.join(
+                    ".", "cdm_reader_mapper", "mdf_reader", "schemas", "icoads"
+                )
+            },
+        ),
+        (
+            "icoads_r300_d703",
+            None,
+            None,
+            None,
+            False,
+            None,
+            {
+                "ext_table_path": os.path.join(
+                    ".", "cdm_reader_mapper", "mdf_reader", "codes", "icoads"
+                )
+            },
+        ),
+        (
+            "icoads_r300_d703",
+            None,
+            None,
+            None,
+            False,
+            None,
+            {
+                "ext_table_path": os.path.join(
+                    ".", "cdm_reader_mapper", "mdf_reader", "codes", "icoads"
+                ),
+                "ext_schema_path": os.path.join(
+                    ".", "cdm_reader_mapper", "mdf_reader", "schemas", "icoads"
+                ),
+            },
+        ),
+        (
+            "icoads_r300_d703",
+            None,
+            None,
+            None,
+            False,
+            None,
+            {
+                "ext_schema_file": os.path.join(
+                    ".",
+                    "cdm_reader_mapper",
+                    "mdf_reader",
+                    "schemas",
+                    "icoads",
+                    "icoads.json",
+                )
+            },
+        ),
+    ],
+)
+def test_read_data(
+    imodel,
+    cdm_subset,
+    codes_subset,
+    out_path,
+    mapping,
+    drops,
+    mdf_kwargs,
+):
+    _testing_suite(
+        **dict(getattr(test_data, f"test_{imodel}")),
+        imodel=imodel,
+        cdm_subset=cdm_subset,
+        codes_subset=codes_subset,
+        out_path=out_path,
+        mapping=mapping,
+        drops=drops,
+        **mdf_kwargs,
+    )
