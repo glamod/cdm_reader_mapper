cc2319557c9eae73afcb7c3da3afbc0b7c9559ef
Ludwig Lierhammer <ludwig.lierhammer@dwd.de>
2024-12-18 11:41:59 +0100

    adjust to CDM

--- tests/test_writers.py
+++ tests/test_writers.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import pandas as pd
 import pytest  # noqa
 
-from cdm_reader_mapper.cdm_mapper import read_tables, write_tables
+from cdm_reader_mapper import read_tables, write_tables
 
 from ._results import result_data
 
@@ -17,33 +17,33 @@ output = read_tables(
 
 
 def test_write_data():
-    write_tables(output, suffix=f"{imodel}_all")
+    output.write_tables(suffix=f"{imodel}_all")
     output_ = read_tables(".", suffix=f"{imodel}_all")
-    pd.testing.assert_frame_equal(output, output_)
+    pd.testing.assert_frame_equal(output.cdm, output_.cdm)
 
 
 @pytest.mark.parametrize("table", ["header", "observations-sst"])
 def test_write_tables(table):
-    write_tables(output, suffix=f"{imodel}_{table}_all", cdm_subset=table)
-    output_table = read_tables(".", suffix=f"{imodel}_{table}_all")
-    output_origi = output[table].dropna(how="all").reset_index(drop=True)
-    pd.testing.assert_frame_equal(output_origi, output_table[table])
+    output.write_tables(suffix=f"{imodel}_{table}_all", cdm_subset=table)
+    output_table = read_tables(".", suffix=f"{imodel}_{table}_all", cdm_subset=table)
+    output_origi = output.cdm[table].dropna(how="all").reset_index(drop=True)
+    pd.testing.assert_frame_equal(output_origi, output_table.cdm[table])
 
 
 def test_write_fns():
-    write_tables(
-        output, prefix="prefix", suffix=f"{imodel}_all", extension="csv", delimiter=","
+    output.write_tables(
+        prefix="prefix", suffix=f"{imodel}_all", extension="csv", delimiter=","
     )
     output_ = read_tables(
         ".", prefix="prefix", suffix=f"{imodel}_all", extension="csv", delimiter=","
     )
-    pd.testing.assert_frame_equal(output, output_)
+    pd.testing.assert_frame_equal(output.cdm, output_.cdm)
 
 
 def test_write_filename():
-    write_tables(output, filename=f"{imodel}_filename_all")
+    output.write_tables(filename=f"{imodel}_filename_all")
     output_ = read_tables(".", suffix=f"{imodel}_filename_all")
-    pd.testing.assert_frame_equal(output, output_)
+    pd.testing.assert_frame_equal(output.cdm, output_.cdm)
 
 
 def test_write_filename_dict():
@@ -51,20 +51,19 @@ def test_write_filename_dict():
         "header": f"{imodel}_filename_dict_all",
         "observations-sst": f"observations-sst-{imodel}_filename_dict_all.psv",
     }
-    write_tables(output, filename=filename_dict)
+    output.write_tables(filename=filename_dict)
     output_ = read_tables(".", suffix=f"{imodel}_filename_dict_all")
-    pd.testing.assert_frame_equal(output[filename_dict.keys()], output_)
+    pd.testing.assert_frame_equal(output.cdm[filename_dict.keys()], output_.cdm)
 
 
 def test_write_col_subset():
     table = "header"
     columns = ["report_id", "latitude", "longitude"]
-    write_tables(
-        output,
+    output.write_tables(
         suffix=f"{imodel}_{table}_all",
         cdm_subset=table,
         col_subset={table: columns},
     )
     output_table = read_tables(".", suffix=f"{imodel}_{table}_all")
-    output_origi = output[table][columns].dropna(how="all").reset_index(drop=True)
-    pd.testing.assert_frame_equal(output_origi, output_table[table])
+    output_origi = output.cdm[table][columns].dropna(how="all").reset_index(drop=True)
+    pd.testing.assert_frame_equal(output_origi, output_table.cdm[table])
