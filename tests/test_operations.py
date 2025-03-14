205eb9e99209654c0ad658ac6446f26af9d6cc43
Ludwig Lierhammer <ludwig.lierhammer@dwd.de>
2025-03-14 08:54:41 +0100

    solve selection tests

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -25,33 +25,31 @@ def _get_data(TextParser, **kwargs):
 def test_select_true(TextParser):
     data = _get_data(TextParser, sections=["c99_data"])
     result = data.select_true(out_rejected=True)
-    selected = result[0]
-    deselected = result[1]
+    expected = data.data
+    selected = result.data
 
     if TextParser is True:
-        data = make_copy(data).read()
+        expected = make_copy(expected).read()
         selected = make_copy(selected).read()
-        deselected = make_copy(deselected).read()
 
-    exp1 = data[:5].reset_index(drop=True)
-    exp2 = data[5:].reset_index(drop=True)
-    pd.testing.assert_frame_equal(exp1, selected)
-    pd.testing.assert_frame_equal(exp2, deselected)
+    expected = expected[:5].reset_index(drop=True)
+    pd.testing.assert_frame_equal(expected, selected)
 
 
 @pytest.mark.parametrize("TextParser", [False, True])
 def test_select_from_index(TextParser):
     data = _get_data(TextParser)
     result = data.select_from_index([0, 2, 4])
-    selected = result[0]
+    expected = data.data
+    selected = result.data
 
     if TextParser is True:
-        data = make_copy(data).read()
+        expected = make_copy(expected).read()
         selected = make_copy(selected).read()
 
-    idx = data.index.isin([0, 2, 4])
-    exp = data[idx].reset_index(drop=True)
-    pd.testing.assert_frame_equal(exp, selected)
+    idx = expected.index.isin([0, 2, 4])
+    expected = expected[idx].reset_index(drop=True)
+    pd.testing.assert_frame_equal(expected, selected)
 
 
 @pytest.mark.parametrize("TextParser", [True, False])
@@ -59,20 +57,16 @@ def test_select_from_list(TextParser):
     data = _get_data(TextParser)
     selection = {("c1", "B1"): [26, 41]}
     result = data.select_from_list(selection, out_rejected=True, in_index=True)
-    selected = result[0]
-    deselected = result[1]
+    expected = data.data
+    selected = result.data
 
     if TextParser is True:
-        data = make_copy(data).read()
+        expected = make_copy(expected).read()
         selected = make_copy(selected).read()
-        deselected = make_copy(deselected).read()
 
-    idx1 = data.index.isin([1, 3])
-    idx2 = data.index.isin([0, 2, 4])
-    exp1 = data[idx1].reset_index(drop=True)
-    exp2 = data[idx2].reset_index(drop=True)
-    pd.testing.assert_frame_equal(exp1, selected)
-    pd.testing.assert_frame_equal(exp2, deselected)
+    idx = expected.index.isin([1, 3])
+    expected = expected[idx].reset_index(drop=True)
+    pd.testing.assert_frame_equal(expected, selected)
 
 
 @pytest.mark.parametrize("TextParser", [True, False])
