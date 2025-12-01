f200ddb62f939fd5a96b9df867ae38d5db3acd65
Ludwig Lierhammer <ludwig.lierhammer@dwd.de>
2025-12-01 11:58:50 +0100

    split tests

--- tests/test_writers.py
+++ tests/test_writers.py
@@ -18,8 +18,18 @@ def test_write_data(tmp_path):
     pd.testing.assert_frame_equal(db_exp.data, db_res.data)
 
 
-@pytest.mark.parametrize("table", ["header", "observations-sst"])
-def test_write_tables(tmp_path, table):
+def test_write_header(tmp_path):
+    table = "header"
+    db_exp.write(out_dir=tmp_path, suffix=f"{imodel}_{table}_all", cdm_subset=table)
+    db_res = read(
+        tmp_path, suffix=f"{imodel}_{table}_all", cdm_subset=table, mode="tables"
+    )
+    table_exp = db_exp[table].dropna(how="all").reset_index(drop=True)
+    pd.testing.assert_frame_equal(table_exp, db_res[table])
+
+
+def test_write_observations(tmp_path):
+    table = "observations-sst"
     db_exp.write(out_dir=tmp_path, suffix=f"{imodel}_{table}_all", cdm_subset=table)
     db_res = read(
         tmp_path, suffix=f"{imodel}_{table}_all", cdm_subset=table, mode="tables"
@@ -53,14 +63,24 @@ def test_write_filename(tmp_path):
     pd.testing.assert_frame_equal(db_exp.data, db_res.data)
 
 
-def test_write_filename_dict(tmp_path):
+def test_write_filename_dict_header(tmp_path):
     filename_dict = {
         "header": f"{imodel}_filename_dict_all",
+    }
+    db_exp.write(out_dir=tmp_path, filename=filename_dict)
+    db_res = read(tmp_path, suffix=f"{imodel}_filename_dict_all", mode="tables")
+    table_exp = db_exp["header"].dropna(how="all").reset_index(drop=True)
+    pd.testing.assert_frame_equal(table_exp, db_res.data["header"])
+
+
+def test_write_filename_dict_observations(tmp_path):
+    filename_dict = {
         "observations-sst": f"observations-sst-{imodel}_filename_dict_all.psv",
     }
     db_exp.write(out_dir=tmp_path, filename=filename_dict)
     db_res = read(tmp_path, suffix=f"{imodel}_filename_dict_all", mode="tables")
-    pd.testing.assert_frame_equal(db_exp[filename_dict.keys()], db_res.data)
+    table_exp = db_exp["observations-sst"].dropna(how="all").reset_index(drop=True)
+    pd.testing.assert_frame_equal(table_exp, db_res.data["observations-sst"])
 
 
 def test_write_col_subset(tmp_path):
