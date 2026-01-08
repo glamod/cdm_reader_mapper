734e2f571058f6352f4b8febfd20fad5456195fb
Ludwig Lierhammer <ludwig.lierhammer@dwd.de>
2026-01-08 15:49:04 +0100

    add unit tests

---
+++ tests/test_reader_filereader.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+import pytest  # noqa
+
+from cdm_reader_mapper.mdf_reader.utils import parser  # noqa
+
+
+def test_filreader():
+    raise NotImplementedError
